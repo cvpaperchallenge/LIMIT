d3afac923d4fe9b4b03a77adf90868b644ec0a4d
gatheluck <gatheluck@gmail.com>
2025-05-26 23:21:39 +0900

    feat: remove base to use subdomain.

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 
-const isProd = process.env.NODE_ENV === 'production';
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [reactRouter(), tailwindcss()],
@@ -13,6 +11,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // Use "/LIMIT/" when building for GitHub Pages, "/" during dev
-  base: isProd ? '/LIMIT/' : '/',
 })
