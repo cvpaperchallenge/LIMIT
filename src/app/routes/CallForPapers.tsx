b52da98874a907caca115afc2c89fd436fd3be73
gatheluck <gatheluck@gmail.com>
2025-06-06 08:01:46 +0900

    feat: add proceeding info.

--- src/app/routes/CallForPapers.tsx
+++ src/app/routes/CallForPapers.tsx
@@ -106,6 +106,10 @@ function CallForPapers() {
               ),
             )}
           </ul>
+          <p>
+            Submission in accepted papers will be included in the official ICCV
+            proceedings.
+          </p>
           <div className="flex gap-4 pt-2">
             <Button variant="outline" className="flex gap-2" asChild>
               <a
